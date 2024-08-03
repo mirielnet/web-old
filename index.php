792e3a97a39b332ae1722046df3c9331b90b6b4b
Miriel <me@miriel.net>
2024-08-03 12:01:32 +0900

    Update index.php

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
         Copyright © 2014-2024 Miriel(@mirielnet) All Rights Reserved.
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         $(document).ready(function() {
