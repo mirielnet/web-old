fa59131539881188366ea7f5bca4d915930fa7c8
Miriel <me@miriel.net>
2024-08-10 18:29:36 +0900

    Feat: Fastifyを使用するように。

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    Copyright © 2014-2024 Miriel(@mirielnet) All Rights Reserved.
-</footer>
-</body>
-</html>
---
+++ index.js
@@ -0,0 +1,27 @@
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+const fastifyStatic = require('fastify-static');
+
+// 静的ファイルを提供するための設定
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/',
+});
+
+// ルートエンドポイントの設定
+fastify.get('/', async (request, reply) => {
+  return reply.sendFile('index.html');
+});
+
+// サーバーの起動
+const start = async () => {
+  try {
+    await fastify.listen(3000, '0.0.0.0');
+    fastify.log.info(`Server is running at http://localhost:3000`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
